f20314c3cdda94abca3f22b599da934252b57e63
takuyawww <wakataku.11010809@gmail.com>
2022-12-25 22:54:39 +0000

    add

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,19 @@
+require 'plugins'
+
+-- basic
 vim.opt.fileencoding = "utf-8"
 vim.opt.cursorline = true
 vim.opt.number = true
+
+vim.cmd 'set autoindent'
+vim.cmd 'set expandtab'
+vim.cmd 'set smartindent'
+vim.cmd 'set clipboard+=unnamedplus'
+vim.cmd 'set noswapfile'
+vim.cmd 'set shiftwidth=2'
+vim.cmd 'set softtabstop=2'
+vim.cmd 'set tabstop=2'
+vim.cmd 'set termguicolors'
+
+-- key mapping
+vim.api.nvim_set_keymap('n', '<C-t>', ':Fern .', { noremap = true, silent = true })
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,10 @@
+require'packer'.startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  
+  -- color scheme
+  use "jacoborus/tender.vim"
+
+  -- file tree
+  use "lambdalisue/fern.vim"
+end)
